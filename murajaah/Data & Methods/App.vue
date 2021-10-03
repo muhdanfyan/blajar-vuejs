7fba92edb910633c0dea8784e6ea8943a0acf60a
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-03 14:18:40 +0800

    data & methods

--- murajaah/Data & Methods/App.vue
+++ murajaah/Data & Methods/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h2>Welcome {{nama}}</h2>
-  <button v-on:click="changeName">Change Name</button>
-  <!-- <button @click="changeName">Change Name</button> -->
+  <!-- <button v-on:click="changeName">Change Name</button> -->
+  <button @click="changeName">Change Name</button>
 </template>
 
 <script>
