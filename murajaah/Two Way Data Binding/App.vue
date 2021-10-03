7f2e7871878e31221601905a748c7da51258a926
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-03 14:27:09 +0800

    two way data binding

---
+++ murajaah/Two Way Data Binding/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="text" v-model="nama" />
+  {{nama}}
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+        nama : "Joko",
+      }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
